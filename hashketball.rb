ef2dae2b6526f867b31755841e2f56b0b2b62a9c
SANJAY K SAI <shachsan@hotmail.com>
2018-10-15 03:06:27 +0000

    Done.

--- hashketball.rb
+++ hashketball.rb
@@ -1,41 +1,110 @@
 def game_hash
-game_hash = {home:
-             {team_name:"Brooklyn Nets" , colors:["Black","White"] , players:
-               {"Alan Anderson" =>
-                 {number: 0, shoe: 16, points: 22, rebounds: 12,
-                  assists: 12, steals: 3, blocks: 1, slam_dunks: 1},
-               "Reggie Evans" =>
-                 {number: 30, shoe: 14, points: 12, rebounds: 12,
-                  assists: 12, steals: 12, blocks: 12, slam_dunks: 7},
-               "Brook Lopez" =>
-                 {number: 11, shoe: 17, points: 17, rebounds: 19,
-                  assists: 10, steals: 3, blocks: 1, slam_dunks: 15},
-               "Mason Plumlee" =>
-                 {number: 1, shoe: 19, points: 26, rebounds: 12,
-                  assists: 6, steals: 3, blocks: 8, slam_dunks: 5},
-               "Jason Terry" =>
-                 {number: 31, shoe: 15, points: 19, rebounds: 2,
-                  assists: 2, steals: 4, blocks: 11, slam_dunks: 1}
+game_hash = {
+    home:{
+        team_name:"Brooklyn Nets",
+        colors:["Black","White"],
+        players:{
+            "Alan Anderson" => {
+                number: 0,
+                shoe: 16,
+                points: 22,
+                rebounds: 12,
+                assists: 12,
+                steals: 3,
+                blocks: 1,
+                slam_dunks: 1
+            },
+            "Reggie Evans" => {
+                number: 30,
+                shoe: 14,
+                points: 12,
+                rebounds: 12,
+                assists: 12,
+                steals: 12,
+                blocks: 12,
+                slam_dunks: 7
+            },
+            "Brook Lopez" => {
+                number: 11,
+                shoe: 17,
+                points: 17,
+                rebounds: 19,
+                assists: 10,
+                steals: 3,
+                blocks: 1,
+                slam_dunks: 15
+            },
+            "Mason Plumlee" =>{
+                number: 1,
+                shoe: 19,
+                points: 26,
+                rebounds: 12,
+                assists: 6,
+                steals: 3,
+                blocks: 8,
+                slam_dunks: 5
+            },
+            "Jason Terry" =>{
+                number: 31,
+                shoe: 15,
+                points: 19,
+                rebounds: 2,
+                assists: 2,
+                steals: 4,
+                blocks: 11,
+                slam_dunks: 1}
                }
              },
 
-           away:
-             {team_name:"Charlotte Hornets" , colors: ["Turquoise", "Purple"] , players:
-               {"Jeff Adrien" =>
-                 {number: 4, shoe: 18, points: 10, rebounds: 1,
-                  assists: 1, steals: 2, blocks: 7, slam_dunks: 2},
-              "Bismak Biyombo" =>
-                {number: 0, shoe: 16, points: 12, rebounds: 4,
-                 assists: 7, steals: 7, blocks: 15, slam_dunks: 10},
-              "DeSagna Diop" =>
-                {number: 2, shoe: 14, points: 24, rebounds: 12,
-                 assists: 12, steals: 4, blocks: 5, slam_dunks:5},
-              "Ben Gordon" =>
-                {number: 8, shoe: 15, points: 33, rebounds: 3,
-                 assists: 2, steals: 1, blocks: 1, slam_dunks: 0},
-              "Brendan Haywood" =>
-                {number: 33, shoe: 15, points: 6, rebounds: 12,
-                 assists: 12, steals: 22, blocks: 5, slam_dunks: 12}
+    away:{
+        team_name:"Charlotte Hornets",
+        colors: ["Turquoise", "Purple"],
+        players:{
+            "Jeff Adrien" => {
+                number: 4,
+                shoe: 18,
+                points: 10,
+                rebounds: 1,
+                assists: 1,
+                steals: 2,
+                blocks: 7,
+                slam_dunks: 2},
+            "Bismak Biyombo" =>{
+                number: 0,
+                shoe: 16,
+                points: 12,
+                rebounds: 4,
+                assists: 7,
+                steals: 7,
+                blocks: 15,
+                slam_dunks: 10},
+            "DeSagna Diop" => {
+                number: 2,
+                shoe: 14,
+                points: 24,
+                rebounds: 12,
+                assists: 12,
+                steals: 4,
+                blocks: 5,
+                slam_dunks:5},
+            "Ben Gordon" => {
+                number: 8,
+                shoe: 15,
+                points: 33,
+                rebounds: 3,
+                assists: 2,
+                steals: 1,
+                blocks: 1,
+                slam_dunks: 0},
+            "Brendan Haywood" => {
+                number: 33,
+                shoe: 15,
+                points: 6,
+                rebounds: 12,
+                assists: 12,
+                steals: 22,
+                blocks: 5,
+                slam_dunks: 12}
                }
              }
           }
@@ -129,4 +198,64 @@ end
 #Implementing Big shoe rebound
 def big_shoe_rebounds
 team_info.max_by {|h| h[:shoe]}[:rebounds]#h[:shoe] returns a hash of stats with the biggest shoe size
+end
+
+
+###Bonus exercises
+
+#Find most_points_scored
+def most_points_scored
+  max_points = team_info.max_by {|stats_hash|stats_hash[:points]}
+  team_info do |team_details|
+    team_details[:players].each do |player, stats|
+      return player if stats == max_points
+    end
+  end
+
+end
+
+
+##helper method for winning_team method to find the total scores of each team
+def find_total_scores(team_details)
+  total_scores = 0
+  team_details[:players].values.each do |stats|
+    total_scores += stats[:points]
+  end
+  return total_scores
+end
+
+
+
+##winning_team
+def winning_team
+  team_scores = Hash.new
+  team_info do |team_details|
+    team_scores[team_details[:team_name]] = find_total_scores(team_details)
+  end
+  team_scores.max_by {|team, score|score}.first
+end
+
+## returns player name with longest characters.
+def player_with_longest_name
+  longest_name = ""
+  team_info do |team_details|
+    team_details[:players]
+    .keys
+    .each do |player_name|
+      if player_name.length > longest_name.length
+        longest_name = player_name
+      end
+    end
+
+  end
+  return longest_name
+end
+
+##returns true or false  if longest name steals a ton
+def long_name_steals_a_ton?
+  highest_steals_players = nil
+  team_info do |team_details|
+    highest_steals_players= team_details[:players].key(team_info.max_by {|stats_hash|stats_hash[:steals]})
+  end
+  highest_steals_players == player_with_longest_name
 end
